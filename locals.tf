2ac68c6f6622d03e0a70e5b80055728d7f4c032a
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2026-06-11 16:49:25 -0400

    Get region

--- locals.tf
+++ locals.tf
@@ -2,3 +2,7 @@ locals {
   region                      = regex("vault-aws-admin-terraform-(.*)", terraform.workspace)
   dynamic_database_configured = local.region == "us-east-2"
 }
+
+output "region" {
+    value = local.region
+}
