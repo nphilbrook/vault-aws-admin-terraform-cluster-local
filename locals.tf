0c6758b455861c31707142c461a1b227e8f49382
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2026-06-11 16:53:01 -0400

    Fix regex return bug

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
-  region                      = regex("vault-aws-admin-terraform-(.*)", terraform.workspace)
+  region                      = regex("vault-aws-admin-terraform-(?P<region>.*)", terraform.workspace)["region"]
   dynamic_database_configured = local.region == "us-east-2"
 }
 
 output "region" {
-    value = local.region
+  value = local.region
 }
